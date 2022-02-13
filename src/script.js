b7bb4164943ac985fc12729a3c32ea81cb8078a3
Lamine ADEL <lamine.adel@ynov.com>
2022-02-13 03:07:58 +0100

    work

--- src/script.js
+++ src/script.js
@@ -1,5 +1,6 @@
 const dino = document.getElementById("dino")
-
+const cactus = document.getElementById("cactus")
+let jumpcount= 0 
 
 
 
@@ -13,6 +14,19 @@ function jump() {
         }, 300)
     }
 }  
+
+let isAlive = setInterval(function(){
+    let dinotop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"))
+    let cactusleft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"))
+
+    //  fonction pour détecter les collisions
+    if(cactusleft <50 && cactusleft > 0 && dinotop >= 140){
+
+        //console.log("collision")
+         alert("Game over!")
+         console.log(jumpcount)
+    }
+},10)
 
 
 document.addEventListener("keydown", function (event) {
